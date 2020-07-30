7ce522a231c8d3eb4bdb560b2d0058fe0d059ecc
UtmoD <tu.dominh2k@gmail.com>
2020-07-30 16:20:02 +0700

    update PSO

--- source/algorithms/so_PSO.py
+++ source/algorithms/so_PSO.py
@@ -3,6 +3,7 @@ from utils.termination.max_time import MaxTime
 from operators.selection.historical_best_selection import HistoricalBestSelection
 import numpy as np
 from numpy.random import uniform
+from numpy.random import rand
 
 class PSO(GA):
     def __init__(self,
@@ -19,10 +20,47 @@ class PSO(GA):
         self.default_termination = MaxTime(5)
         if not topology in ['star', 'ring']:
             raise Exception('Population Topology not found!')
+        self.global_best_selection = self.star_topology_selection
+        if topology != 'star':
+            self.global_best_selection = self.ring_topology_selection
         self.selection = selection
         self.initialization = initialization
         self.intertia_weight = iw
         self.accel_const = ac
+        self.global_best = None
+
+    def star_topology_selection(self):
+        argopt = self.problem._argopt
+        return argopt(self.f_pop_prev, axis=0)
+
+    def ring_topology_selection(self):
+        optimum = self.problem._optimum
+        indices = []
+        n = self.pop_size
+        f_p = self.f_pop_prev
+        for i in range(n-1):
+            neighbors = (f[i-1], f[i], f_p[i+1])
+            elite_idx = neighbors.index(optimum(neighbors))
+            indices.append(elite_idx + (i-1))
+
+        neighbors_last = (f_p[n-2], f_p[n-1], f_p[0])
+        idx_last = neighbors_last.index(optimum(neighbors_last))
+        if idx_last != 2:
+            indices.append(idx_last + n-2)
+        else:
+            indices.append(0)
+        return indices
+
+    def compute_velocity(self):
+        r_p, r_g = rand(), rand()
+        c1, c2 = self.accel_const, self.accel_const
+        w = self.intertia_weight
+        p = self.pop_prev
+        P = self.pop
+        g = self.global_best
+        v = self.velocity
+        new_v = w*v + c1*r_p * (p-P) + c2*r_g * (g-P)
+        return new_v
 
     def _initialize(self):
         self.pop_prev = self.pop.copy()
@@ -36,8 +74,19 @@ class PSO(GA):
         self.f_pop = self.evaluate(self.pop)
         self.f_pop_prev = self.evaluate(self.pop_prev)
 
-        prev_best = self.selection._do(self)
-        indices = np.arange(self.pop_size)
+        prev_best_idx = set(self.selection._do(self))
+        indices = set(np.arange(self.pop_size))
+        current_best_idx = indices - prev_best_idx
+        self.pop_prev = self.pop[current_best_idx]
+        self.f_pop_prev = self.f_pop[current_best_idx]
+
+        global_best_idx = self.global_best_selection()
+        self.global_best = self.pop_prev[global_best_idx]
+
+        self.velocity = self.compute_velocity()
+        self.pop = (self.pop + self.velocity).astype(self.problem.param_type)
+
+
 
 
 
