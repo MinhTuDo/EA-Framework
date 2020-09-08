c1ff5474a14ee8c7284167c39b66590f86891e1e
UtmoD <tu.dominh2k@gmail.com>
2020-09-08 15:17:57 +0700

    update 08/09

--- ea/load_pickle.py
+++ ea/load_pickle.py
@@ -21,78 +21,93 @@ import algorithms.multi_objective as mo
 
 np.set_printoptions(suppress=True)
 
-filename = './log/obj_20200905-1357_gen-1__NSGAII-CustomizedNSGANET_.pickle'
-algorithm = None
+filename = './log/obj_20200908-0413_gen-2__NSGAII-CustomizedNSGANET_.pickle'
+gen2 = None
 with open(filename, 'rb') as handle:
-    algorithm = pickle.load(handle)
+    gen2 = pickle.load(handle)
 
-class MySaver(LogSaver):
-    def _do(self, ga):
-        self.add_attributes('architecture', ga.pop[ga.elite_idx])
-        self.add_attributes('error_rate', ga.F_pop[ga.elite_idx])
-        self.add_attributes('flops', ga.F_pop[ga.elite_idx])
-        self.add_attributes('n_gens', ga.n_gens) 
-        self.add_attributes('n_evals', ga.n_evals) 
+filename = './log/obj_20200905-1357_gen-1__NSGAII-CustomizedNSGANET_.pickle'
+gen1 = None
+with open(filename, 'rb') as handle:
+    gen1 = pickle.load(handle)
+
+plt.plot(gen2.ranks_F[0][:, 1]/10, gen2.ranks_F[0][:, 0]*100, 'b.', label='gen-2')
+plt.plot(gen1.ranks_F[0][:, 1]/10, gen1.ranks_F[0][:, 0]*100, 'r.', label='gen-1')
+plt.xlabel('flops (MB)')
+plt.ylabel('error rate (%)')
+plt.legend(loc='upper right')
+plt.grid(True, linestyle='--')
+plt.show()
+
+
+
+# class MySaver(LogSaver):
+#     def _do(self, ga):
+#         self.add_attributes('architecture', ga.pop[ga.elite_idx])
+#         self.add_attributes('error_rate', ga.F_pop[ga.elite_idx])
+#         self.add_attributes('flops', ga.F_pop[ga.elite_idx])
+#         self.add_attributes('n_gens', ga.n_gens) 
+#         self.add_attributes('n_evals', ga.n_evals) 
         
-        now = time.time()
-        self.add_attributes('time', now - ga.start_time)
+#         now = time.time()
+#         self.add_attributes('time', now - ga.start_time)
         
 
-class MyDisplay(Display):
-    def _do(self, ga):
-        self.display_top = -1
-        self.add_attributes('n_gens', ga.n_gens) 
-        self.add_attributes('n_evals', ga.n_evals) 
-        self.add_attributes('error_rate', ga.F_pop[ga.elite_idx])
-        self.add_attributes('flops', ga.F_pop[ga.elite_idx])
-        now = time.time()
-        self.add_attributes('time', now - ga.start_time)
-        self.add_attributes('architecture', ga.pop[ga.elite_idx])
+# class MyDisplay(Display):
+#     def _do(self, ga):
+#         self.display_top = -1
+#         self.add_attributes('n_gens', ga.n_gens) 
+#         self.add_attributes('n_evals', ga.n_evals) 
+#         self.add_attributes('error_rate', ga.F_pop[ga.elite_idx])
+#         self.add_attributes('flops', ga.F_pop[ga.elite_idx])
+#         now = time.time()
+#         self.add_attributes('time', now - ga.start_time)
+#         self.add_attributes('architecture', ga.pop[ga.elite_idx])
 
-display = MyDisplay()
-log_saver = MySaver()
+# display = MyDisplay()
+# log_saver = MySaver()
 
-algorithm.display = display
-algorithm.log_saver = log_saver
+# algorithm.display = display
+# algorithm.log_saver = log_saver
 
 
-NSGANET = mp.NSGANET
-class CustomizedNSGANET(NSGANET):
-    def _preprocess_input(self):
-        x_train, y_train = self.train_data
-        x_train = x_train.astype('float32')
-        x_train /= 255
-        y_train = to_categorical(y_train, self.n_classes)
+# NSGANET = mp.NSGANET
+# class CustomizedNSGANET(NSGANET):
+#     def _preprocess_input(self):
+#         x_train, y_train = self.train_data
+#         x_train = x_train.astype('float32')
+#         x_train /= 255
+#         y_train = to_categorical(y_train, self.n_classes)
 
-        x_test, y_test = self.test_data
-        x_test = x_test.astype('float32')
-        x_test /= 255
-        y_test = to_categorical(y_test, self.n_classes)
+#         x_test, y_test = self.test_data
+#         x_test = x_test.astype('float32')
+#         x_test /= 255
+#         y_test = to_categorical(y_test, self.n_classes)
         
-        self.train_data = (x_train, y_train)
-        self.test_data = (x_test, y_test)
+#         self.train_data = (x_train, y_train)
+#         self.test_data = (x_test, y_test)
 
-    def _model_fit(self):
-        self.model.fit(x=self.train_data[0],
-                       y=self.train_data[1],
-                       epochs=self.epochs,
-                       verbose=2)
+#     def _model_fit(self):
+#         self.model.fit(x=self.train_data[0],
+#                        y=self.train_data[1],
+#                        epochs=self.epochs,
+#                        verbose=2)
 
-    def _model_evaluate(self):
-        [_, validate_acc] = self.model.evaluate(self.test_data[0], self.test_data[1])
-        return validate_acc
+#     def _model_evaluate(self):
+#         [_, validate_acc] = self.model.evaluate(self.test_data[0], self.test_data[1])
+#         return validate_acc
 
-train_data, test_data = cifar10.load_data()    
-problem = CustomizedNSGANET(input_shape=(32, 32, 3),
-                              n_classes=10,
-                              data=(train_data, test_data, None),
-                              activation_last='softmax',
-                              optimizer='sgd',
-                              loss='categorical_crossentropy',
-                              stages=(6, 6),
-                              activation='relu',
-                              epochs=25)
+# train_data, test_data = cifar10.load_data()    
+# problem = CustomizedNSGANET(input_shape=(32, 32, 3),
+#                               n_classes=10,
+#                               data=(train_data, test_data, None),
+#                               activation_last='softmax',
+#                               optimizer='sgd',
+#                               loss='categorical_crossentropy',
+#                               stages=(6, 6),
+#                               activation='relu',
+#                               epochs=25)
 
-algorithm.problem = problem
+# algorithm.problem = problem
 
-algorithm.run_continue()
+# algorithm.run_continue()
