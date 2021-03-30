51ba44824f2fc550bf058337616165e36ca22bde
2Dooh <tu.dominh2k@gmail.com>
2021-03-30 16:57:28 +0700

    update run ux

--- old_test.py
+++ old_test.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 np.set_printoptions(suppress=True)
 
-filename = './logs/run_1_seed_1.pickle'
+filename = './logs/run_2_seed_0.pickle'
 gen = None
 with open(filename, 'rb') as handle:
     gen = pickle.load(handle)
@@ -13,7 +13,7 @@ gen_12 = gen[23]
 
 #gen_last = gen[-1]
 
-filename = 'logs/obj_20210323-1403_gen-24__NSGAII-NSGANet_.pickle'
+filename = 'logs/obj_20210327-0456_gen-25__NSGAII-NSGANet_.pickle'
 init_gen = None
 with open(filename, 'rb') as handle:
     init_gen = pickle.load(handle)
@@ -21,8 +21,8 @@ with open(filename, 'rb') as handle:
 print(gen_12.ranks_F[0][0])
 print(gen_12.ranks[0][0])
 
-plt.plot(gen_12.F_pop[:, 1], gen_12.F_pop[:, 0], 'b.', label='mb 24')
-plt.plot(init_gen.F_pop[:, 1], init_gen.F_pop[:, 0], 'r.', label='ux 24')
+plt.plot(gen_12.F_pop[:, 1], gen_12.F_pop[:, 0], 'b.', label='mb 25')
+plt.plot(init_gen.F_pop[:, 1], init_gen.F_pop[:, 0], 'r.', label='ux 25')
 # plt.plot(gen_last.F_pop[:, 1], gen_last.F_pop[:, 0], 'g.', label='mb last')
 # last_sorted_idx = np.argsort(last_gen.ranks_F[0][:, 1])
 # init_sorted_idx = np.argsort(init_gen.ranks_F[0][:, 1])
--- rerun.py
+++ rerun.py
@@ -2,7 +2,7 @@ import pickle5 as pickle
 from utils.displayer import NSGANetDisplay
 from utils.logger import NSGANetLogSaver
 
-filename = './logs/obj_20210305-1248_gen-2__NSGAII-NSGANet_.pickle'
+filename = 'logs/obj_20210323-1403_gen-24__NSGAII-NSGANet_.pickle'
 
 with open(filename, 'rb') as handle:
     algorithm = pickle.load(handle)
